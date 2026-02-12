7d92ebe5d750e95647ce8281c79b553da669e8cb
VSHNeer Tea <259125310+vshntea@users.noreply.github.com>
2026-02-12 15:52:40 +0100

    Hash environments and annotate to trigger pod restarts on changes

--- pkg/converter/patcher.go
+++ pkg/converter/patcher.go
@@ -1,9 +1,13 @@
 package converter
 
 import (
+	"fmt"
+	"hash/fnv"
+	"sort"
 	"strings"
 
 	apps "k8s.io/api/apps/v1"
+	core "k8s.io/api/core/v1"
 )
 
 func addAnnotations(annotations *map[string]string, addAnnotations map[string]string) {
@@ -33,26 +37,94 @@ func isModifiedImage(image string, modifiedImages []string) bool {
 	return false
 }
 
-func PatchDeployments(deployments []apps.Deployment, modifiedImages []string, forceRestartAnnotation map[string]string) {
-	// don't use 'range', getting a pointer to an array element does not work with 'range'
-	for i := 0; i < len(deployments); i++ {
-		for _, container := range deployments[i].Spec.Template.Spec.Containers {
-			if isModifiedImage(container.Image, modifiedImages) {
-				addAnnotations(&deployments[i].Spec.Template.Annotations, forceRestartAnnotation)
-				break
-			}
+func getSecretByName(secrets []core.Secret, name string) (*core.Secret, error) {
+	for i := range secrets {
+		if secrets[i].Name == name {
+			return &secrets[i], nil
 		}
+	}
+	return nil, fmt.Errorf("secret %q not found", name)
+}
+
+// As environments could potentionally be very long we concatenate everything and hash at the end
+// We sort beforehand to not depend on the order of key-value entries
+func hashSecret(secret *core.Secret) uint32 {
+	sortedKeys := make([]string, 0, len(secret.StringData))
+	for k := range secret.StringData {
+		sortedKeys = append(sortedKeys, k)
+	}
+	sort.Strings(sortedKeys)
+
+	// Concatenate all key-value pairs
+	hashInput := []byte{}
+	for _, k := range sortedKeys {
+		hashInput = append(hashInput, []byte(k)...)
+		hashInput = append(hashInput, secret.Data[k]...)
+	}
+
+	hash := fnv.New32a()
+	hash.Write([]byte(hashInput))
+	return hash.Sum32()
+}
+
+// We use XOR to not depend on the order of the secrets and not cause an overflow
+func hashSecrets(secrets []*core.Secret) uint32 {
+	var hash uint32 = 0
+	for _, secret := range secrets {
+		hash = hash ^ hashSecret(secret)
+	}
+	return hash
+}
+
+func patchPodTemplate(template *core.PodTemplateSpec, secrets []core.Secret, modifiedImages []string, forceRestartAnnotation map[string]string) {
+	matchingSecrets := []*core.Secret{}
+	modified := false
+	for _, container := range template.Spec.Containers {
+		for _, env := range container.EnvFrom {
+			if env.SecretRef == nil {
+				continue
+			}
+			secret, err := getSecretByName(secrets, env.SecretRef.Name)
+			if err != nil {
+				continue
+			}
+			matchingSecrets = append(matchingSecrets, secret)
+		}
+		for _, env := range container.Env {
+			if env.ValueFrom == nil || env.ValueFrom.SecretKeyRef == nil {
+				continue
+			}
+			secret, err := getSecretByName(secrets, env.ValueFrom.SecretKeyRef.Name)
+			if err != nil {
+				continue
+			}
+			matchingSecrets = append(matchingSecrets, secret)
+		}
+		if isModifiedImage(container.Image, modifiedImages) {
+			modified = true
+		}
+	}
+	hash := hashSecrets(matchingSecrets)
+	if hash != 0 {
+		addAnnotations(&template.Annotations, map[string]string{
+			"k8ify.restart-trigger-config": fmt.Sprint(hash),
+		})
+	}
+	if modified {
+		addAnnotations(&template.Annotations, forceRestartAnnotation)
 	}
 }
 
-func PatchStatefulSets(statefulSets []apps.StatefulSet, modifiedImages []string, forceRestartAnnotation map[string]string) {
+func PatchDeployments(deployments []apps.Deployment, modifiedImages []string, secrets []core.Secret, forceRestartAnnotation map[string]string) {
+	// don't use 'range', getting a pointer to an array element does not work with 'range'
+	for i := 0; i < len(deployments); i++ {
+		patchPodTemplate(&deployments[i].Spec.Template, secrets, modifiedImages, forceRestartAnnotation)
+	}
+}
+
+func PatchStatefulSets(statefulSets []apps.StatefulSet, modifiedImages []string, secrets []core.Secret, forceRestartAnnotation map[string]string) {
 	// don't use 'range', getting a pointer to an array element does not work with 'range'
 	for i := 0; i < len(statefulSets); i++ {
-		for _, container := range statefulSets[i].Spec.Template.Spec.Containers {
-			if isModifiedImage(container.Image, modifiedImages) {
-				addAnnotations(&statefulSets[i].Spec.Template.Annotations, forceRestartAnnotation)
-				break
-			}
-		}
+		patchPodTemplate(&statefulSets[i].Spec.Template, secrets, modifiedImages, forceRestartAnnotation)
 	}
 }
